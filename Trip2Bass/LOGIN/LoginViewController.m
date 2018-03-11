deaf6fd66830621821fd1083317eec6883354458
Jonathan Torres <proview1996@Jonathans-Mac.local>
2018-03-11 13:43:17 -0700

    Login de Usuario Completo.

--- Trip2Bass/LOGIN/LoginViewController.m
+++ Trip2Bass/LOGIN/LoginViewController.m
@@ -44,30 +44,6 @@
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 
-/*
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    //Inicializamos la BD.
-    DBManager* db = [[DBManager alloc] initWithDatabaseFilename:@"Trip2Bass.sqlite"];
-    //Recogemos el usuario y la contraseña de los TextField.
-    NSString* usuario = self.tfUsername.text;
-    NSString* password = self.tfPassword.text;
-    
-    //Comprobamos si el usuario existe en la BD.
-    if([db validarUsuario:usuario conPassword:password]){
-        if ([[segue identifier] isEqualToString:@"loguea"]) {
-            
-            UITabBarController *tabBarController = segue.destinationViewController;
-            UINavigationController *navigationController = (UINavigationController *)[[tabBarController viewControllers] objectAtIndex:0];
-            EventosCollectionVC *controller = (EventosCollectionVC *)[[navigationController viewControllers] objectAtIndex:0];
-            
-            controller.username = self.tfUsername.text;
-        }
-    }else{
-        NSLog(@"No se encuentra el usuario");
-    }
-}
-*/
-
 - (BOOL)shouldPerformSegueWithIdentifier:(NSString *)identifier sender:(id)sender {
     //Inicializamos la BD.
     DBManager* db = [[DBManager alloc] initWithDatabaseFilename:@"Trip2Bass.sqlite"];
@@ -77,11 +53,32 @@
     
     //Comprobamos si el usuario existe en la BD.
     if([db validarUsuario:usuario conPassword:password]){
-        NSLog(@"validado y entramos");
+        //Si existe mandamos el prepareForSegue y mostramos la siguiente vista.
+        [self performSegueWithIdentifier:@"loguea" sender:sender];
         return YES;
     }else{
-        NSLog(@"No se encuentra el usuario");
+        
+        //ALERTA DE USUARIO NO ENCONTRADO
+        UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Usuario o contraseña incorrectos" message:@"Si no tienes una cuenta createla apretando el boton de Sign Up" preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"Cerrar" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {}];
+        
+        [alert addAction:defaultAction];
+        [self presentViewController:alert animated:YES completion:nil];
+        
         return NO;
+    }
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+   
+    if ([[segue identifier] isEqualToString:@"loguea"]) {
+        
+        UITabBarController *tabBarController = segue.destinationViewController;
+        UINavigationController *navigationController = (UINavigationController *)[[tabBarController viewControllers] objectAtIndex:0];
+        EventosCollectionVC *controller = (EventosCollectionVC *)[[navigationController viewControllers] objectAtIndex:0];
+        
+        controller.username = self.tfUsername.text;
     }
 }
 
